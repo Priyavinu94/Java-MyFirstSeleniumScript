506506259e28f1ea3834db615013981935843504
Priya <sreepdsy1994@gmail.com>
2022-03-13 22:08:57 -0400

    Add Assert

--- src/test/java/com/sreepriyavinod/learningSelenium/AutomationPractice.java
+++ src/test/java/com/sreepriyavinod/learningSelenium/AutomationPractice.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -52,6 +53,13 @@ public class AutomationPractice {
 		//sendQuery.submit(); //----not working
 		
 		sendQuery.click(); 	//clicks Send button
+		
+		//using getText() and assertEquals() to check if the Success alert displays as expected.
+		WebElement successMessage = driver.findElement(By.cssSelector("p[class='alert alert-success']"));
+		Assert.assertEquals(successMessage.getText(), "Your message has been successfully sent to our team.", "Unexpected Success alert");
+		
+		//to check if the test fails for the below:--- it is failing---
+		Assert.assertEquals(successMessage.getText(), "Message successfully sent.", "Unexpected Success alert");
 
 	}
 	
