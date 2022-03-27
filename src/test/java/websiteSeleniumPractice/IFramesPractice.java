db91334e543ab9d2a2043c651a4aa9a5d2d48d18
Priya <sreepdsy1994@gmail.com>
2022-03-26 21:50:46 -0400

    Practice webTable

---
+++ src/test/java/websiteSeleniumPractice/IFramesPractice.java
@@ -0,0 +1,53 @@
+package websiteSeleniumPractice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class IFramesPractice {
+	
+	public WebDriver driver;
+
+	@BeforeMethod
+	public void openBrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Driver\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get("http://seleniumpractise.blogspot.com/2017/01/frames-in-selenium-webdriver.html");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void switchToIframe() {
+		
+		//Finding iframe element
+		WebElement iFrameElement = driver.findElement(By.id("google"));
+		
+		//switch to iframe
+		driver.switchTo().frame(iFrameElement);
+		
+		//finding an element inside iframe and clicking on it
+		WebElement handlePopupLink = driver.findElement(By.cssSelector("article.post-5276 a.entry-title-link"));
+		handlePopupLink.click();
+		
+		String text = driver.findElement(By.cssSelector("#genesis-content div.breadcrumb")).getText();
+		System.out.println(text);
+		//Assertion
+		Assert.assertEquals(text.contains("Handle Authentication Pop Up"), true);
+		
+		//switch to parent page and assert
+		driver.switchTo().defaultContent();
+
+		WebElement headingElement = driver.findElement(By.xpath("//h3[@itemprop='name']"));
+		Assert.assertEquals(headingElement.getText(), "Frames in Selenium WebDriver");
+		
+	}
+}
---
+++ src/test/java/websiteSeleniumPractice/webTablesPractice.java
@@ -0,0 +1,56 @@
+package websiteSeleniumPractice;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class webTablesPractice {
+
+	public WebDriver driver;
+
+	@BeforeMethod
+	public void openBrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Driver\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get("http://seleniumpractise.blogspot.com/2021/08/webtable-in-html.html");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void switchToIframe() {
+
+		// xpath : //table[@id='customers']//tbody//tr[1]//th[1]
+
+		List<WebElement> rowsList = driver.findElements(By.cssSelector("#customers tr"));
+		// locator using xpath : //table[@id='customers']//tbody//tr
+
+		List<WebElement> columnsList = driver.findElements(By.cssSelector("#customers tr th"));
+		// locator using xpath : //table[@id='customers']//tbody//tr//th
+
+		String beforeIndex = "//table[@id='customers']//tbody//tr[";
+		String afterIndex = "]//td[";
+
+		// iterating through table elements and printing
+		for (int i = 2; i <= rowsList.size(); i++) {
+			for (int j = 1; j <= columnsList.size(); j++) {
+				WebElement tableElement = driver.findElement(By.xpath(beforeIndex + i + afterIndex + j + "]"));
+				if (!tableElement.getText().equals("")) {
+					System.out.println(tableElement.getText());
+				} else {
+					System.out.println("------------ ");
+				}
+			}
+		}
+
+	}
+
+}
